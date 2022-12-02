0f7405657fb988e3a165a19bf8028f13b5f34160
Stanley Kim <kim4111@purdue.edu>
2022-12-02 13:49:55 -0500

    Finished Option 3, 4, 11 for Customer Options
    Finished Option 1 and 10 for Seller Options

--- SellerServer.java
+++ SellerServer.java
@@ -3,14 +3,17 @@ import java.io.*;
 import java.util.ArrayList;
 
 public class SellerServer {
+
+
+    // Seller Option 2
     public static void createNewItem() {
         try {
             ArrayList<String> lines = SearchServer.getTextInfo(new File("Markets.txt"));
             String[] markets = new String[lines.size()];
             for (int i = 0; i < markets.length; i++) {
                 markets[i] = lines.get(i);
-
             }
+
 
 
             JFrame jFrame = new JFrame("Create new Product");
